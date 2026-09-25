c208ac30bad96fdf5fd41f45dca7924e8ae4f3ae
eleve1n <eleve1n@yahoo.com>
2026-09-25 19:55:27 +0530

    Rmeove Broken variables

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,10 +21,6 @@ TARGET_NO_BOOTLOADER := true
 # Display
 TARGET_SCREEN_DENSITY := 480
 
-# Vintf
-BUILD_BROKEN_VINTF_PRODUCT_COPY_FILES := true
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml
